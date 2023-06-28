361d1ced74ad13def361be975eef6a7aea9b770b
Gabriel Santos <82126990+Lucio072@users.noreply.github.com>
2023-06-28 12:17:49 -0300

    pages

---
+++ src/app/pages/anexo/anexo.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Map, tileLayer } from 'leaflet';
+@Component({
+  selector: 'app-anexo',
+  templateUrl: './anexo.page.html',
+  styleUrls: ['./anexo.page.scss'],
+})
+export class AnexoPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+
+}
---
+++ src/app/pages/location/location.page.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Map, tileLayer } from 'leaflet';
+@Component({
+  selector: 'app-location',
+  templateUrl: './location.page.html',
+  styleUrls: ['./location.page.scss'],
+})
+export class LocationPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+
+  neAfterViewInit(): void {
+    const map = new Map('map').setView([51.505, -0.09], 13);
+    tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution:
+        '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(map);
+  }
+}
