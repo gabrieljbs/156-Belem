bef8e41fcf022f69492c7813c11ef819e3bfaf6e
Gabriel Santos <82126990+Lucio072@users.noreply.github.com>
2023-07-24 07:04:42 -0300

    'visual'

--- src/app/pages/ticket/ticket.page.ts
+++ src/app/pages/ticket/ticket.page.ts
@@ -12,8 +12,8 @@ import { StorageService } from 'src/app/services/storage.service';
 export class TicketPage implements OnInit {
   public imageSrc: any;
   public loading: any;
-  public location: any = {lat:'', lon:''}
-  public description = ''
+  public location: any = { lat: '', lon: '' };
+  public description = '';
   private state: any;
   public name: any;
   public icon: any;
@@ -24,42 +24,74 @@ export class TicketPage implements OnInit {
     private route: ActivatedRoute,
     private solicitation: SolicitationService,
     private storageService: StorageService
-  ) {
-
-  }
+  ) {}
   ngOnInit() {
-
     this.state = this.router.getCurrentNavigation()?.extras.state;
-    console.log(this.state)
-
+    console.log(this.state);
   }
 
   async showLoading() {
     this.loading = await this.loadingCtrl.create();
-     this.loading.present();
-   }
+    this.loading.present();
+  }
 
-   async takePicture() {
+  async takePicture() {
     const image = await Camera.getPhoto({
       quality: 90,
       allowEditing: true,
-      resultType: CameraResultType.Uri
+      resultType: CameraResultType.Uri,
     });
     const imageUrl = image.webPath;
     this.imageSrc = imageUrl;
-  };
-
+  }
 
   customCounterFormatter(inputLength: number, maxLength: number) {
     return `${maxLength - inputLength} Caracteres`;
   }
 
-  async ticket(descricao:string, input: any){
+  async ticket(descricao: string, input: any) {
     await this.storageService.setFiles(input);
-    await this.solicitation.create(descricao,this.state.location,this.state.icon,this.state.name)
+    await this.solicitation.create(
+      descricao,
+      this.state.location,
+      this.state.icon,
+      this.state.name
+    );
     this.router.navigate(['/home']);
-
   }
 
+  async handleFileChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    const files = inputElement.files;
 
+    if (files && files.length > 0) {
+      const file = files[0];
+
+      if (file.type.startsWith('image/')) {
+        this.showImagePreview(file);
+      } else if (file.type.startsWith('video/')) {
+        this.showVideoPreview(file);
+      }
+    }
+  }
+
+  showImagePreview(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      this.imageSrc = (event.target as FileReader).result;
+    };
+
+    reader.readAsDataURL(file);
+  }
+
+  showVideoPreview(file: File) {
+    const reader = new FileReader();
+
+    reader.onload = (event) => {
+      this.imageSrc = (event.target as FileReader).result;
+    };
+
+    reader.readAsDataURL(file);
+  }
 }
