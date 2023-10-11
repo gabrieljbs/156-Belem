9f479c6b3b7a5eeb0b4abf213f346de712834ead
Gabriel Santos <gabrieljbs@outlook.pt>
2023-10-10 23:36:35 -0300

    Ajustando estilo do cadastro

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, ToastController } from '@ionic/angular';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { AuthService } from 'src/app/services/auth.service';
 @Component({
@@ -9,28 +10,62 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-  public user = {
-    email: '',
-    password: '',
-  };
+  public userForm: FormGroup
   private loading: any;
+  public error: string = '';
   constructor(
     private authService: AuthService,
     private loadingCtrl: LoadingController,
-    private router: Router
-  ) {}
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private toastController: ToastController
+  ) {
+    this.userForm = this.formBuilder.group({
+      full_name:'',
+      email: '',
+      password: '',
+      confirmPassword:''
+    });
+  }
 
   ngOnInit() {}
 
-  async userRegister(user: any) {
-    this.showLoading();
-    await this.authService.register(user);
-    this.loading.dismiss();
-    this.router.navigate(['/login'])
+  async register() {
+    try{
+      this.showLoading();
+      await this.authService.register(this.userForm.value);
+      this.loading.dismiss();
+      this.redirect('/login')
+    } catch(error: any){
+      console.log(error.message)
+      this.loading.dismiss();
+      return await this.presentToast(error.message);
+    }
   }
 
   async showLoading() {
    this.loading = await this.loadingCtrl.create();
     this.loading.present();
+  }
+
+  redirect(path:string){
+    this.router.navigate([path]);
+  }
+
+  async presentToast(e: string) {
+    /* if (e === 'Firebase: Error (auth/invalid-email).') {
+      this.error = 'Email é inválido';
+    } else if (e === 'Firebase: Error (auth/wrong-password).') {
+      this.error = 'Senha incorreta';
+    }
+ */
+    const toast = await this.toastController.create({
+      message: e,
+      duration: 7000,
+      position: 'bottom',
+      color: 'danger',
+    });
+
+    await toast.present();
   }
 }
