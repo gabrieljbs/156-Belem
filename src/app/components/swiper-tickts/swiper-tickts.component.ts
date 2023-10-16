b831f3244d0992a31df7aa5e0ea7e4164415331c
Gabriel Santos <gabrieljbs@outlook.pt>
2023-10-16 17:58:18 -0300

    Corrgir identação

--- src/app/components/swiper-tickts/swiper-tickts.component.ts
+++ src/app/components/swiper-tickts/swiper-tickts.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
 import { SolicitationService } from 'src/app/services/solicitation.service';
 import { SolicitationDetailsComponent } from 'src/app/components/modals/solicitation-details/solicitation-details.component';
@@ -14,7 +13,10 @@ export class SwiperTicktsComponent implements OnInit {
   public isLoading = false;
   public interfaceCard: any[] = [];
 
-  constructor(private solicitation: SolicitationService,private router: Router, private modalCtrl: ModalController) {}
+  constructor(
+    private solicitation: SolicitationService,
+    private modalCtrl: ModalController
+  ) {}
 
   async ngOnInit() {
     try {
@@ -22,24 +24,22 @@ export class SwiperTicktsComponent implements OnInit {
       const slids = await this.solicitation.read();
       slids.forEach((doc) => {
         this.interfaceS.push(doc.data());
-      });
-      const buttom = await this.solicitation.card();
-      buttom.forEach((doc) => {
-        this.interfaceCard.push(doc.data());
-      });
 
+      });
       this.isLoading = false;
-    } catch (error) {}
+    } catch (error) {
+      this.isLoading = false;
+
+    }
   }
 
-  async openModalSolicitation(data:any) {
+  async openModalSolicitation(data: any) {
     const modal = await this.modalCtrl.create({
       component: SolicitationDetailsComponent,
       componentProps: {
-        data
-      }
+        data,
+      },
     });
     return await modal.present();
-
   }
 }
