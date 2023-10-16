d43550d1a08cc889fb048bff9e055336d640a205
Gabriel Santos <gabrieljbs@outlook.pt>
2023-10-15 23:37:05 -0300

    removendo console.log

--- src/app/pages/new-solicitation/new-solicitation.page.ts
+++ src/app/pages/new-solicitation/new-solicitation.page.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { SolicitationService } from 'src/app/services/solicitation.service';
-
 @Component({
   selector: 'app-new-solicitation',
   templateUrl: './new-solicitation.page.html',
@@ -11,15 +8,11 @@ export class NewSolicitationPage implements OnInit {
   public interfaceCard: any[] = [];
   public results: string = '';
   public isLoading = false;
-  constructor(
-    private solicitation: SolicitationService,
-    private router: Router
-  ) {}
+  constructor() {}
 
   ngOnInit() {}
   handleInput(event: any) {
     const query = event.target.value.toLowerCase();
-    this.results = query
-    console.log(this.results)
+    this.results = query;
   }
 }
