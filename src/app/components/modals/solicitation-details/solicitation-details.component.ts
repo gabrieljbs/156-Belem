526ba81a9b574bfb867aee13603e4211331599b4
Gabriel Santos <gabrieljbs@outlook.pt>
2023-10-16 17:57:45 -0300

    Ajuste de stylo

--- src/app/components/modals/solicitation-details/solicitation-details.component.ts
+++ src/app/components/modals/solicitation-details/solicitation-details.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, AlertController } from '@ionic/angular';
+import { SolicitationService} from './../../../services/solicitation.service';
 @Component({
   selector: 'app-solicitation-details',
   templateUrl: './solicitation-details.component.html',
@@ -8,16 +9,63 @@ import { ModalController } from '@ionic/angular';
 export class SolicitationDetailsComponent  implements OnInit {
   @Input() data: any;
 
-  constructor(private modalCtrl: ModalController) {}
+  constructor(
+    private modalCtrl: ModalController,
+    private alertController: AlertController,
+    private services: SolicitationService
+    ) {}
 
-  cancel() {
-    return this.modalCtrl.dismiss(null, 'cancel');
-  }
+    public alertButtons = [
+      {
+      text: 'Confirmar',
+      role: 'confirm',
+      handler: async() => {
+        console.log(this.data)
+        await this.services.delete(this.data.uid)
+      },
+    },, 'Cancelar'];
 
-  confirm() {
-    return this.modalCtrl.dismiss(null, 'confirm');
-  }
+    public alertInputs = [
+    {
+      label: 'Me enganei',
+      type: 'radio',
+      value: 'Me enganei',
+      handler: () => {
+        console.log(this.alertInputs.values);
+      },
+    },
+    {
+      label: 'Já resolvido',
+      type: 'radio',
+      value: 'Já resolvido',
+      handler: () => {
+        console.log(this.alertInputs.values);
+      },
+    },
+    {
+      label: 'Localização errada',
+      type: 'radio',
+      value: 'Localização errada',
+      handler: () => {
+        console.log(this.alertInputs.values);
+      },
+    },
+    {
+      label: 'Foto errada',
+      type: 'radio',
+      value: 'Foto errada',
+      handler: () => {
+        console.log(this.alertInputs.values);
+      },
+    }
+  ];
+
   ngOnInit() {
+    console.log(this.data)
+  }
+
+  close(){
+    this.modalCtrl.dismiss()
   }
 
 }
