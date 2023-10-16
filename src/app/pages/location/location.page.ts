19b3b0ccbfb7b51c2a43ba7fac4f13802a4df695
Gabriel Santos <gabrieljbs@outlook.pt>
2023-10-16 17:59:02 -0300

    Ajuste na passagem de dados

--- src/app/pages/location/location.page.ts
+++ src/app/pages/location/location.page.ts
@@ -1,60 +1,105 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Control, Map, latLng, marker, tileLayer } from 'leaflet';
-import 'leaflet-control-geocoder';
+import { Router } from '@angular/router';
+import { ToastController } from '@ionic/angular';
+import * as L from 'leaflet';
+import { GeoSearchControl, OpenStreetMapProvider } from 'leaflet-geosearch';
+import 'leaflet.locatecontrol';
+
 @Component({
   selector: 'app-location',
   templateUrl: './location.page.html',
   styleUrls: ['./location.page.scss'],
 })
 export class LocationPage implements OnInit {
-  public location: any  = {lat:'', lon:''}
-  public icon:any;
-  public name: any
+  public input: any;
+  public icon: any;
+  public name: any;
   private state: any;
+  private location: any = {lat:'',lng:''};
+  private local: any;
+
   constructor(
     private router: Router,
-    private route: ActivatedRoute,
+    private toastController: ToastController
   ) {}
 
   ngOnInit() {
-
     this.state = this.router.getCurrentNavigation()?.extras?.state;
-    this.icon = this.state.icon
-    this.name = this.state.name
-    console.log(this.state)
+    this.icon = this.state.icon;
+    this.name = this.state.name;
   }
 
   ngAfterViewInit(): void {
+    const map = L.map('map').setView([-1.45502, -48.5024], 16);
 
-    const map = new Map('map').setView([-1.45502, -48.5024], 13);
-    tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+    L.tileLayer(
+      "https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png",
+      {
+        maxZoom: 19,
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+      }
+    ).addTo(map);
 
-    }).addTo(map);
+    const searchControl = GeoSearchControl({
+      provider: new OpenStreetMapProvider(),
+      style: 'bar',
+      position: 'topleft',
+      searchLabel: this.local ? this.local : 'Insira o endereço...',
+      notFoundMessage: 'Desculpa, endereço não encontrado.',
+      showMarker: false,
+      showPopup: false,
+    });
+    map.addControl(searchControl).getCenter();
 
+    const searchEventHandler = (result: any) => {
+    };
 
-    map.locate();
+    map.on('geosearch/showlocation', searchEventHandler);
+    L.control
+      .locate({
+        position: 'topleft',
+        drawCircle: true
 
-    const onLocationFound = (e: any) => {
-      const radius = e.accuracy;
-      this.location.lat = e.latlng.lat;
-      this.location.lon = e.latlng.lng;
-      console.log(this.location.lat,this.location.lon)
+      })
+      .addTo(map);
 
-      marker(e.latlng)
-      .addTo(map)
-      .bindPopup("Você está a " + radius + " metros aproximadamente")
-      .openPopup();
+      let currentMarker:any = null;
+
+      map.on('click', (e) => {
+        this.location.lat = e.latlng.lat
+        this.location.lng = e.latlng.lng
+        console.log(this.location)
+        if (currentMarker) {
+          map.removeLayer(currentMarker);
+        }
+        currentMarker = L.marker(this.location).addTo(map);
+      });
+  }
+
+  ticket() {
+    if (this.location.lat! || this.location.lng!) {
+      this.router.navigate([`ticket`], {
+        state: {
+          icon: this.state.icon,
+          name: this.state.name,
+          lat: this.location.lat,
+          lon: this.location.lng,
+        },
+      });
+    } else {
+      this.presentToast();
     }
-
-    map.on('locationfound', onLocationFound);
-
-
   }
 
-  ticket(){
-    this.router.navigate([`ticket`], {state:{icon:this.state.icon, name: this.state.name, lat:this.location.lat, lon:this.location.lon}});
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Informe a localização',
+      duration: 3000,
+      position: 'bottom',
+      color: 'danger',
+    });
+
+    await toast.present();
   }
-
-
 }
